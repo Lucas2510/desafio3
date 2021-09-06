7e584b90e6163bcb198476b8ffcead905a37d34b
jonlopez01 <jonathan.lopez01@gmail.com>
2021-09-06 10:57:19 -0300

    return List<Product>, controller ResponseEntity

--- src/main/java/desafio/devoptopus/desafio3/controller/ProductController.java
+++ src/main/java/desafio/devoptopus/desafio3/controller/ProductController.java
@@ -1,12 +1,16 @@
 package desafio.devoptopus.desafio3.controller;
 
+import desafio.devoptopus.desafio3.document.Product;
 import desafio.devoptopus.desafio3.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -16,8 +20,8 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/product")
-    public ResponseEntity<Object> findProducts(@RequestParam("search") String search) {
-        return productService.findProducts(search);
+    public ResponseEntity<List<Product>> findProducts(@RequestParam("search") String search) {
+        return new ResponseEntity<>( productService.findProducts(search), HttpStatus.OK);
     }
 
 }
--- src/main/java/desafio/devoptopus/desafio3/service/implementation/ProductServiceImpl.java
+++ src/main/java/desafio/devoptopus/desafio3/service/implementation/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package desafio.devoptopus.desafio3.service.implementation;
 
+import desafio.devoptopus.desafio3.document.Product;
 import desafio.devoptopus.desafio3.repository.ProductRepository;
 import desafio.devoptopus.desafio3.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,22 +8,32 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ProductServiceImpl implements ProductService {
 
     @Autowired
     ProductRepository productRepository;
 
-    public ResponseEntity<Object> findProducts(String search) {
+    public List<Product> findProducts(String search) {
         try {
             Long id = Long.parseLong(search);
-            return new ResponseEntity<>(productRepository.findById(id), HttpStatus.OK);
+            List<Product> listProduct = new ArrayList<Product>();
+            Product product = productRepository.findById(id);
+            listProduct.add(product);
+            //return new ResponseEntity<Product>(listProduct, HttpStatus.OK);
+
+            return listProduct;
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        if (search.length() < 3) {
-            return new ResponseEntity<>("error", HttpStatus.BAD_REQUEST);
+        if (search.length() >= 3) {
+            return productRepository.findByRegex(search);
         }
-        return new ResponseEntity<>(productRepository.findByRegex(search), HttpStatus.OK);
+        List<Product> listProduct = new ArrayList<Product>();
+        return listProduct;
+
     }
 }
