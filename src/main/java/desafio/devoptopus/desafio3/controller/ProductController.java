f8fe36e046a4e3eec37653e555d140dd35dca5b6
criscastrov <85178299+criscastrov@users.noreply.github.com>
2021-09-08 16:45:46 -0300

    Control error/handler error (#5)
    * [Feature]HandleError
    
    -Cambios en el timeout de Mongo
    -creación de Clase ApiError para mejor control de errores
    -WIP control de errores
    
    * [Feature]HandleError
    
    -Actualización de ApiError
    -Añadido ErrorAdvice
    -Trabajo en progeso

--- src/main/java/desafio/devoptopus/desafio3/controller/ProductController.java
+++ src/main/java/desafio/devoptopus/desafio3/controller/ProductController.java
@@ -6,7 +6,11 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
@@ -22,7 +26,7 @@ public class ProductController {
             "products that match the entered data, to return a list of products",nickname = "products")
     @GetMapping("/products")
     public ResponseEntity<List<Product>> findProducts(@RequestParam("search") String search) {
-        return new ResponseEntity<>( productService.findProducts(search), HttpStatus.OK);
+        return new ResponseEntity<>(productService.findProducts(search), HttpStatus.OK);
     }
 
 }
